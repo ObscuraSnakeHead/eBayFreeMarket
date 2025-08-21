16a7a482d62f94d4c02ca36dc2f4a25803b9c16c
ObscuraSnakeHead <tristen.martinez@gmx.co.uk>
2025-08-21 13:04:09 -0400

    Create mattermost.go

---
+++ modules/bots/mattermost.go
@@ -0,0 +1,20 @@
+package bots
+
+import (
+	"github.com/mattermost/mattermost-server/model"
+)
+
+type MattermostBot struct {
+	Client *model.Client4
+}
+
+func NewMattermostBot(url string) MattermostBot {
+	mattermostBot := MattermostBot{
+		Client: model.NewAPIv4Client(url),
+	}
+	return mattermostBot
+}
+
+// func ParseMattermostCommand() {
+// 	model.NewAPIv4Client
+// }
